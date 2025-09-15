6f2234e32be5102cc0281c75a450145750719388
Siddharth Rajan <sidbraj@gmail.com>
2025-09-15 17:58:30 -0500

    working implementation of the script.py is now finished

--- app/script.py
+++ app/script.py
@@ -7,6 +7,13 @@ import sqlite3
 import copy
 from transformers import pipeline
 import torch
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv("keys.env")
+apiKey = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key = apiKey)
 
 #FOR NOW WILL REMOVE THE APP ROUTING TO TEST CAPABILITY OF SCRIPT IN ITSELF
 conn = sqlite3.connect('userData.db')
@@ -72,15 +79,17 @@ def handle_post(data):
     #commented out for now
     #data = request.json
     username = data["username"]
-    calsPerDay = data["calsEaten"] - data["calsBurned"]
+    calsPerDay = data["calsTotal"]
     socialMeidaHours = data["socialMediaHrs"]
     waterDrank = data["waterDrank"]
     hrsProductive = data["hrsProductive"]
     cursor.execute('SELECT * FROM userInfo WHERE username = ? ', [username])
     row = cursor.fetchone()
     if (row):
-        cursor.execute('INSERT INTO userInfo (calsIntaked, socialMediaHours, waterDrank, hrsProductive) VALUES (?, ?, ?, ?)', 
-            [calsPerDay, socialMeidaHours, waterDrank, hrsProductive])
+        cursor.execute('''UPDATE userInfo 
+                       SET calsIntaked = ?, socialMediaHours = ?, waterDrank = ?, hrsProductive = ?
+                       WHERE username = ?''', 
+            [calsPerDay, socialMeidaHours, waterDrank, hrsProductive, username])
         conn.commit()
         return 1
     else:
@@ -120,12 +129,12 @@ def handleDailyPost(data):
     #vals to return:
     outputs = {
         "dailyInfo": {},
-        "weelyInfo": ""
+        "weeklyInfo": ""
     }
     
     #insert into DB:
-    cursor.execute('INSERT INTO dailyInfo (username, calsIntaked, socialMediaHours, waterDrank, hrsProductive)', 
-                    [username, dailyData["cals"], dailyData["socialMediaHrs"], 
+    cursor.execute('INSERT INTO dailyInfo (username, calsIntaked, socialMediaHours, waterDrank, hrsProductive) VALUES (?, ?, ?, ?, ?)', 
+                    [username, dailyData["calsIntaked"], dailyData["socialMediaHours"], 
                      dailyData["waterDrank"], dailyData["hrsProductive"]])
     conn.commit()
 
@@ -133,38 +142,49 @@ def handleDailyPost(data):
     def handleDifferences(actual, expected, toNormalDifferences):
         percentDiff = lambda actual, expected: (abs(actual-expected) / expected) * 100
         for i in range(0, len(actual)):
-            if actual[i] < expected[i]:
-                difference = percentDiff(actual[i], expected[i])
-                toNormalDifferences[(list(toNormalDifferences.keys())[0])] += difference
-
+            #this is for the calculations within the calories intakes and social media hours, where the  less, the better
+            if (i<2):
+                if actual[i] < expected[i]:
+                    difference = percentDiff(actual[i], expected[i])
+                    toNormalDifferences[(list(toNormalDifferences.keys())[0])] += difference
+                else:
+                    difference = percentDiff(actual[i], expected[i])
+                    toNormalDifferences[(list(toNormalDifferences.keys())[0])] -= difference
+                    toNormalDifferences[(list(toNormalDifferences.keys())[i+1])] += difference
+            #This is for the calculations within the water drank and hours productive where the more, the better
             else:
-                difference = percentDiff(actual[i], expected[i])
-                toNormalDifferences[(list(toNormalDifferences.keys())[0])] -= difference
-                toNormalDifferences[(list(toNormalDifferences.keys())[i])] += difference
+                if actual[i] > expected[i]:
+                    difference = percentDiff(actual[i], expected[i])
+                    toNormalDifferences[(list(toNormalDifferences.keys())[0])] += difference
+                else:
+                    difference = percentDiff(actual[i], expected[i])
+                    toNormalDifferences[(list(toNormalDifferences.keys())[0])] -= difference
+                    toNormalDifferences[(list(toNormalDifferences.keys())[i+1])] += difference
+        toNormalDifferences[(list(toNormalDifferences.keys())[0])] /= 4
+        return toNormalDifferences
     
     #populate daily dicts   
-    cursor.execute('SELECT * FROM userInfo WHERE username = ?', (username))
+    cursor.execute('SELECT * FROM userInfo WHERE username = ?', [username])
     row = cursor.fetchone()
     if (row):
-        actual = [dailyData["cals"], dailyData["socialMediaHrs"],  dailyData["waterDrank"], dailyData["hrsProductive"]]
+        actual = [dailyData["calsIntaked"], dailyData["socialMediaHours"],  dailyData["waterDrank"], dailyData["hrsProductive"]]
         expected = [row["calsIntaked"], row["socialMediaHours"], row["waterDrank"], row["hrsProductive"]]
-        handleDifferences(actual, expected, dailyToNormalDifferences)
+        dailyToNormal = handleDifferences(actual, expected, dailyToNormalDifferences)
         #populate throttle
-        for keys in dailyToNormalDifferences.keys():
-            if dailyToNormalDifferences[keys] > 15 and dailyToNormalDifferences[keys] < 30:
-                dailyToNormalThrottle["one"].append(keys)
-            elif dailyToNormalDifferences[keys] > 30 and dailyToNormalDifferences[keys] < 50:
-                dailyToNormalThrottle["two"].append(keys)
-            elif dailyToNormalDifferences[keys] >= 50:
-                dailyToNormalThrottle["three"].append(keys)
-        
+        for key in dailyToNormalDifferences.keys():
+            if dailyToNormal[key] > 15 and dailyToNormal[key] < 30:
+                dailyToNormalThrottle["one"].append(key)
+            elif dailyToNormal[key] > 30 and dailyToNormal[key] < 50:
+                dailyToNormalThrottle["two"].append(key)
+            elif dailyToNormal[key] >= 50:
+                dailyToNormalThrottle["three"].append(key)
         outputs["dailyInfo"] = dailyToNormalThrottle
-        
     #Extract information about weekly information
-    cursor.execute('SELECT * FROM dailyInfo WHERE username = ? ORDER BY id DESC LIMIT 1', (username))
+    cursor.execute('SELECT * FROM dailyInfo WHERE username = ? ORDER BY id DESC LIMIT 1', [username])
     row = cursor.fetchone()
     if (row):
-        if (row["id"] >= 7 and row["id"]%7 == 0):
+        #Commenting this down to 0, replace bace to 7 when done with testing
+        if (row["id"] >= 0): # and row["id"]%7 == 0):
             cursor.execute('SELECT * FROM dailyInfo WHERE id > ? AND id < ? AND username = ?', 
                             [row["id"]-7, row["id"], username])
             rows = cursor.fetchall()
@@ -177,58 +197,62 @@ def handleDailyPost(data):
                 weeklyData[key] /= 7
             #Iterate through keys in weeklyAvergae, find username, find diff b/w expected and 
             #weekly avg, place into throttle sections
-            cursor.execute('SELECT * FROM userInfo WHERE username = ?', (username))
+            cursor.execute('SELECT * FROM userInfo WHERE username = ?', [username])
             row = cursor.fetchone()
             expected = [row["calsIntaked"], row["socialMediaHours"], row["waterDrank"], row["hrsProductive"]]
             actual = [weeklyData["calsIntaked"], weeklyData["socialMediaHours"], weeklyData["waterDrank"], 
                       weeklyData["hrsProductive"]]
-            handleDifferences(actual, expected, weeklyToNormalDiferences)
-            #Pulling informative AI prompts. Will video-rendering in future, text rendering for now
+            weeklyDifferencesAnswer = handleDifferences(actual, expected, weeklyToNormalDiferences)
             prompt = (
                 f"Weekly summary of user '{username}' :\n"
-                f'''Deviation between expected calories intaked and actual calories intaked is {weeklyToNormalDiferences["calsIntakedDeviation"]}
+                f'''Deviation between expected calories intaked and actual calories intaked is {weeklyDifferencesAnswer["calsIntakedDeviation"]}
                     Where the expects is above the actual'''
-                f'''Deviation between expected Social Media Hours and actual social media hours is {weeklyToNormalDiferences["socialMediaDeviation"]}
+                f'''Deviation between expected Social Media Hours and actual social media hours is {weeklyDifferencesAnswer["socialMediaDeviation"]}
                     Where the actual is above the expected'''
-                f'''Deviation between expected water consumed and actual water consumed is {weeklyToNormalDiferences["watersConsumedDeviation"]}
+                f'''Deviation between expected water consumed and actual water consumed is {weeklyDifferencesAnswer["watersConsumedDeviation"]}
                     Where the actual is below the expected'''
-                f'''Deviation between expected hours productive and actual hours productive is {weeklyToNormalDiferences["hoursProductiveDeviation"]}
+                f'''Deviation between expected hours productive and actual hours productive is {weeklyDifferencesAnswer["hoursProductiveDeviation"]}
                     Where the actual is below the expected'''
-                f'''Deviation between expected health/productivity activites and actual health/productivity activites is {weeklyToNormalDiferences["hoursProductiveDeviation"]}
+                f'''Deviation between expected health/productivity activites and actual health/productivity activites is {weeklyDifferencesAnswer["TotalNormalizedDiff"]}
                     Where the actual is above OR below the expected'''
                 "Based on this information, please provide a detailed and comprehensive action plan, which motivates the user, showing them common " \
                 "reasons for these flaws (especially comparing values which are very negative, and which may be contribting to the overall negative)" \
                 "health/productivity scores"
             )
-            model_id = "openai/gpt-oss-120b"
-            pipe = pipeline(
-                "text-generation",
-                model=model_id,
-                torch_dtype="auto",
-                device_map="auto",
+
+            response = client.responses.create(
+                model="gpt-4o-mini",
+                input=prompt
             )
-            messages = [
-                {"role": "user", 
-                 f"content": {prompt}},
-            ]
-            ans = pipe(
-                messages,
-                max_new_tokens=600,
-            )
-            outputs["weelyInfo"] = ans
+            outputs["weeklyInfo"] += response.output_text
     return outputs
 
 if __name__ == '__main__':
-    data = {
-        "username": "sid",
-        "calsEaten": 10, 
-        "socialMediaHrs": 1,
-        "calsBurned": 200,
-        "waterDrank": 4,
-        "hrsProductive": 8
+    
+    registrationData = {
+        "username": "Luke",
+        "password": "12345"
     }
-    val = handle_post(data)
-    print(val)
+    firstTimData = {
+        "username": "Luke",
+        "calsTotal": 2100,
+        "socialMediaHrs": 3,
+        "waterDrank": 8,
+        "hrsProductive": 7
+    }
+    dailyData = {
+        "username": "Luke",
+        "calsEaten": 40000, 
+        "calsBurned": 80,
+        "socialMediaHrs": 9,
+        "waterDrank": 3,
+        "hrsProductive": 5
+    }
+    val3 = handleDailyPost(dailyData)
+    print("THIS IS THE DAILY INFO")
+    print(val3["dailyInfo"])
+    print("THIS IS THE WEEKLY INFO")
+    print(val3["weeklyInfo"])
     #app.run(host='0.0.0.0', port=5001)
 
 
